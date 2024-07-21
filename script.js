a60fcd1127e63010a4bcd0994913d4748cc817c3
Ghostified <bransonallan@gmail.com>
2024-07-21 13:49:40 +0300

    completed the random password generator function

---
+++ script.js
@@ -0,0 +1,28 @@
+const passWordBox = document.getElementById("password");
+//password length
+const length = 12;
+
+//define characters that are allowed in the password
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const specialSymbols = "@#$%^&*()_+~|{}[]<>?/=-";
+
+const allCharacters = upperCase + lowerCase + number + specialSymbols;
+
+//Function to generate the random password by selecting randomly
+function createPassword(){
+    let password = " ";
+    password += upperCase[Math.floor(Math.random()* upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() *lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += specialSymbols[Math.floor(Math.random() * specialSymbols.length)];
+
+    //Ensure the password is length is equivalent to the defined length = 12
+    while (length > password.length){
+        password += allCharacters[Math.floor(Math.random() * allCharacters.length)];
+    }
+
+    //Display the randomly generated password
+    passWordBox.value = password;
+}
